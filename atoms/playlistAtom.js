6c00327cb4b61bd3ceba336d47b7eacf3663d75b
Rocky627 <rockyjoe627@gmail.com>
2022-03-21 23:24:05 +0530

    Initialized

---
+++ atoms/playlistAtom.js
@@ -0,0 +1,11 @@
+import { atom } from "recoil";
+
+export const playlistState = atom({
+    key: "playlistState",
+    default: null
+})
+
+export const playlistIdState = atom({
+    key: "playlistIdState",
+    default: "5tvoX7B9PRvyj3Bsv1E8SV"
+});
